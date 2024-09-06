9942e10c6dc291cf3a49d846ca661f6095e651a8
Alvaro Caviedes <94495729+CalciferXe@users.noreply.github.com>
2024-09-06 10:01:43 -0500

    (Ejemplo Clase)
    Es un ejemplo de lo que se esta avanzando en clase ( esta sujeto a modificaciones)

---
+++ frontend_appsalud/lib/configs/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor = Color(0xfff26e22);
+  static const Color secondaryColor = Color(0xFFC3C57D);
+  static const Color backgroundColor = Color(0xFFA5B4C2);
+  static const Color textColor = Color(0xFF5067A8);
+}
--- frontend_appsalud/lib/main.dart
+++ frontend_appsalud/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'configs/colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,72 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
       home: Scaffold(
-        body: Center(
-          child: Text('Bienvenido a la aplicación de control de Salud!'),
+        appBar: AppBar(
+          title: Text('App ULIMA'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Text(
+                  'Mis Cursos',
+                  style: _textTheme,
+                  textAlign: TextAlign.start, // Added textAlign property
+                ),
+                Card(
+                  color: AppColors.primaryColor,
+                  shadowColor: AppColors.secondaryColor,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        'https://www.ulima.edu.pe/sites/default/files/faculty/image/industrial_2_copy.jpg',
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10, top: 15, bottom: 15, right: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '600054',
+                              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w300),
+                            ),
+                            Text(
+                              'Programación Móvil - 872',
+                              style: _textStyle,
+                            ),
+                            Text(
+                              'Activo',
+                              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500, color: Colors.green),
+                            ),
+                            Container(
+                              height: 2.0,
+                              color: Colors.black,
+                              margin: EdgeInsets.symmetric(vertical: 5.0),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+// Defined constants for TextStyle
+final _textTheme = TextStyle(fontSize: 22, fontWeight: FontWeight.w600);
+final _textStyle = TextStyle(fontWeight: FontWeight.w500);
